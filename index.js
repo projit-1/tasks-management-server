4cbf739132745a6407fa9a12fc4e48d6ad1856a5
projit-1 <projitmondol@gmail.com>
2022-07-02 02:38:14 +0600

    insert task form is done

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ async function run() {
             res.send(tasks);
         })
 
+        //post
+
+        app.post('/task', async (req, res) => {
+            const newTask = req.body;
+            const result = await taskCollection.insertOne(newTask);
+            res.send(result);
+        })
+
+
     }
     finally {
 
